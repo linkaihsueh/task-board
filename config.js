14a5d2ba3a84f4bb3afe9a5117a1fd26e3bb340e
linkaihsueh <t204204187su3@gmail.com>
2026-08-13 22:56:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ↓↓↓ 把引號中間換成你的 Google Apps Script 網址，然後存檔。整個設定就只有這一行。 ↓↓↓
-var BOARD_API = "";
+var BOARD_API = "https://script.google.com/macros/s/AKfycbwcVvhO2TWvPG8QYAA6bbokk9nh4a6josC_gZAFKaYug66ZAYfdbnvIdacuBeYAuERD/exec";
 
 // 網址長得像這樣：https://script.google.com/macros/s/AKfycb.....省略...../exec
